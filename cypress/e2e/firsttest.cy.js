13a2e1cc7daf7fe59ea7e515e9f3c69822f2ee6b
Ashok Kumar Shrestha <59673063+ashokstha61@users.noreply.github.com>
2025-07-13 10:53:04 +0545

    first test

---
+++ cypress/e2e/firsttest.cy.js
@@ -0,0 +1,15 @@
+// describe('Login scenario',()=>{
+//     it('Does not do much',()=>{
+//         expect(true).to.equal(true)
+//     })
+// })
+
+/// <reference types="cypress" />
+describe("Lunch Application", () => {
+  it("lunch the application", () => {
+    cy.visit("https://www.youtube.com");
+  });
+});
+
+
+
